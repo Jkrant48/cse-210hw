7f069d31ed86157e826d20fd4ec1c10dd3133904
Jonathan Koranteng <kor22002@byui.edu>
2023-04-27 05:13:46 +0000

    Finished perp 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,52 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int number = -99999;
+        Console.WriteLine("Enter a series of positive numbers (type 0 to finish)");
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            string response = Console.ReadLine();
+            number = int.Parse(response);
+            
+            
+            if (number != 0)
+            {
+                numbers.Add(number);                
+            }
+
+        }
+        // sum
+        int total = 0;
+        foreach (int num in numbers)
+        {
+            total += num;    
+        }
+        Console.WriteLine($"The sum is: {total}");
+
+        // Average
+        float average = ((float)total) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // max number
+        int max_number = -1;
+        
+        foreach (int num in numbers)
+        {
+            if (num > max_number)
+            {
+                max_number = num;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max_number}");
+
+
+
+        
     }
 }
