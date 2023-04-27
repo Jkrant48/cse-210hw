7f7f27c87a9aaa950963d907c73bfb4026b45db7
Jonathan Koranteng <kor22002@byui.edu>
2023-04-27 04:36:57 +0000

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,37 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,100);
+
+        //Console.Write("What is the magic number? ");
+        //string response1 = Console.ReadLine();
+        //int answer = int.Parse(response1);
+        
+        int guesses = 0;
+        int answer1 = -5;
+        while (answer1 != number)
+        {
+            Console.Write("What is your guess? ");
+            guesses += 1; 
+            string response2 = Console.ReadLine();
+            answer1 = int.Parse(response2);
+
+            if (answer1 < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (answer1 > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (answer1 == number)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        Console.WriteLine($"You guessed {guesses} times.");
+
     }
 }
